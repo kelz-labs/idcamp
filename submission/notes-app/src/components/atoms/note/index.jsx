09346691e11a597d36d3e19540a4abc06fdef7f7
haikelz <haikel.2017th@gmail.com>
2022-07-13 04:05:12 +0700

    add submission

--- submission/notes-app/src/components/atoms/note/index.jsx
+++ submission/notes-app/src/components/atoms/note/index.jsx
@@ -12,7 +12,7 @@ const Note = ({ editNote, deleteNote, filteredSearch }) => {
           <p className="font-bold mb-1">
             Keterangan: <span className="font-medium">{note.keterangan}</span>
           </p>
-          <div className="flex gap-4">
+          <div className="flex mt-2 gap-4">
             <button
               onClick={() => editNote(note.id)}
               className="bg-rose-400 hover:bg-rose-500 focus:border-2 duration-300 transition focus:border-blue-500 px-4 py-2 font-bold rounded-md text-white"
